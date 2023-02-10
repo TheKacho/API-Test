ef6ed60acc805ccc625c4f0c071cb8c2bcd56641
TheKacho <abyssknightzero@gmail.com>
2023-02-10 14:22:08 -0500

    added baseURL, needs site url to test

--- API Test/UnitTest1.cs
+++ API Test/UnitTest1.cs
@@ -1,11 +1,29 @@
+
+using Newtonsoft.Json;
+using Xunit.Abstractions;
+
 namespace API_Test
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly ITestOutputHelper output;
 
+        public UnitTest1(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public async Task ApiTest()
+        {
+            string baseUrl = "";
+            HttpClient client= new HttpClient();
+            client.BaseAddress = new Uri(baseUrl);
+
+            var response = await client.GetAsync("Insert/info/here");
+            var content = await response.Content.ReadAsStringAsync();
+
+            var data = JsonSerializer.Deserialize<ResponseModel>(content);
         }
     }
 }
