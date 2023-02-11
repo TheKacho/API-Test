a436bd8b7924b4a0f8bba849db7e1016fa89c18a
TheKacho <abyssknightzero@gmail.com>
2023-02-11 14:39:55 -0500

    added url to first test

--- API Test/UnitTest1.cs
+++ API Test/UnitTest1.cs
@@ -1,6 +1,9 @@
 
 using Newtonsoft.Json;
 using Xunit.Abstractions;
+using FluentAssertions.Execution;
+using FluentAssertions;
+using Xunit.Sdk;
 
 namespace API_Test
 {
@@ -16,14 +19,21 @@ namespace API_Test
         [Fact]
         public async Task ApiTest()
         {
-            string baseUrl = "";
+            string baseUrl = "https://www.valvesoftware.com/";
             HttpClient client= new HttpClient();
             client.BaseAddress = new Uri(baseUrl);
 
-            var response = await client.GetAsync("Insert/info/here");
+            var response = await client.GetAsync("about/stats");
             var content = await response.Content.ReadAsStringAsync();
 
             var data = JsonSerializer.Deserialize<ResponseModel>(content);
+            var usersActive = int.Parse(data.users_active, System.Globalization.NumberStyles.AllowThousands);
+
+            using(new AssertionScope())
+            {
+                response.IsSuccessStatusCode.Should().BeTrue();
+                usersActive.Should().BeGreaterThan(0);
+            }
         }
     }
 }
